fb60044c0104dff2ded226355bc5b2aac6cf48cb
Your-Full-Name <wesllyakatsuka@gmail.com>
2022-06-16 17:00:33 -0300

    Assinatura de métodos com retorno

--- Program.cs
+++ Program.cs
@@ -43,6 +43,7 @@ namespace CSharp
                 // Classes e Métodos
                 {"Membros - Classes e Métodos", Membros.Executar},
                 {"Construtores - Classes e Métodos", Construtores.Executar},
+                {"Métodos com Retorno - Classes e Métodos", MetodosComRetorno.Executar},
 
 
             });
