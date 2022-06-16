50f1cf92be3140a57434d99e30d17efc58b2017a
Your-Full-Name <wesllyakatsuka@gmail.com>
2022-06-16 15:47:32 -0300

    Diferentes maneiras de utilizar o Construtor

---
+++ ClassesEMetodos/Construtores.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CSharp.ClassesEMetodos
+{
+    class Carro
+    {
+        public string Fabricante;
+        public string Modelo;
+        public int Ano;
+
+        public Carro(string fabricante, string modelo, int ano)
+        {
+            Fabricante = fabricante;
+            Modelo = modelo;
+            Ano = ano;
+        }
+
+        public Carro()
+        {
+
+        }
+    }
+
+
+    class Construtores
+    {
+        public static void Executar()
+        {
+            var carro1 = new Carro();
+            carro1.Fabricante = "BMW";
+            carro1.Modelo = "325i";
+            carro1.Ano = 2017;
+            Console.WriteLine($"{carro1.Fabricante} {carro1.Modelo} {carro1.Ano}");
+
+            var carro2 = new Carro("Ka", "Ford", 2018);
+            Console.WriteLine($"{carro2.Fabricante} {carro2.Modelo} {carro2.Ano}");
+
+            var carro3 = new Carro()
+            {
+                Fabricante = "Fiat",
+                Modelo = "Uno",
+                Ano = 2019
+            };
+            Console.WriteLine($"{carro3.Fabricante} {carro3.Modelo} {carro3.Ano}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -42,6 +42,7 @@ namespace CSharp
 
                 // Classes e Métodos
                 {"Membros - Classes e Métodos", Membros.Executar},
+                {"Construtores - Classes e Métodos", Construtores.Executar},
 
 
             });
